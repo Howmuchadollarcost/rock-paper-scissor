bccc63cd06a22a28796921093d30c7906b655f69
Howmuchadollarcost <muluyonas@yahoo.com>
2019-06-21 10:07:14 +0200

    initialize

---
+++ main.js
@@ -0,0 +1,73 @@
+const choices = document.querySelectorAll('.choice');
+
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+
+const resultComp = document.querySelector('#resultComp');
+const win = document.querySelector('#win');
+
+const btn = document.querySelector('button');
+
+
+btn.style.display = 'none';
+
+const scoreBoard = {
+    round: 0,
+    p: 0,
+    c: 0,
+    t:0
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', (e) => {
+        // console.log(e.target.id);
+        const player = e.target.id;
+        const computer = computerGuess();
+        const winner = isWin(player, computer);
+        changeScoreBoard(winner,computer);
+        btn.style.display = '';
+    })
+})
+
+
+function computerGuess() {
+    const rand = Math.random();
+    return (rand < 0.34) ? "rock" : (rand < 0.67 ? "paper" : "scissor");
+}
+
+function isWin(p, c) {
+    if (p === c) {
+        scoreBoard.t++;
+        return "draw";
+    } else if (p === "rock" && c === "paper" || p === "paper" && c === "scissor" || p === "scissor" && c === "rock") {
+        scoreBoard.c++;
+        return "computer wins"
+    } else if (c === "rock" && p === "paper" || c === "paper" && p === "scissor" || c === "scissor" && p === "rock") {
+        scoreBoard.p++;
+        return "player wins"
+    }else{
+        return "Wrong"
+    }
+}
+
+
+function changeScoreBoard(winner,c){
+    player.innerHTML = `<p>Player : ${scoreBoard.p}`
+    computer.innerHTML = `<p>Computer : ${scoreBoard.c}`
+    ties.innerHTML = `<p>Ties : ${scoreBoard.t}`
+    resultComp.innerHTML = `<p>Computer Chose : ${c}`
+
+    if(winner === "player wins"){
+        win.innerHTML = `<h1 id="win">You win</h1>`
+        win.style.color =  "#00a04a";
+    }else if(winner === "computer wins"){
+        win.innerHTML = `<h1 id="win">You Lose</h1>`
+        win.style.color =  "#e01f3d";
+    }else{
+        win.innerHTML = `<h1 id="win">Tie</h1>`
+        win.style.color = "#37333e";
+    }
+}
+
+
+btn.addEventListener('click', () =>{location.reload()});
